959eef255d97a9d158783b1beaf4e7dc9af9da07
adrienbaron <adrien@abaron.net>
2018-08-22 16:03:42 +0200

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'cos-pwa',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Clash of Stats PWA'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: {color: '#3B8070'},
+  /*
+  ** Build configuration
+  */
+  build: {
+    extractCSS: true
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        asset-name-collision-bug
+      </h1>
+      <div class="links">
+        <h2>Should be big:</h2>
+        <img src="~/assets/big/archer.png" alt="">
+        <h2>Should be small:</h2>
+        <img src="~/assets/small/archer.png" alt="">
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
+
